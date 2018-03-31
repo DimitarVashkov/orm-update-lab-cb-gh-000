c01f110e824ab21fa1342398817dbaa9794d939d
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-31 21:03:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,6 +54,12 @@ attr_accessor :name, :grade, :id
    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
+  def create(name,grade)
+    student = Student.new(name,grade)
+    student.save
+    student
+  end
+
 
 
 end
